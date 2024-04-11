b36ae8e4c08b823de60e7f2a5a71fbc3cdc6ec8d
guoconde <guoconde@gmail.com>
2024-04-11 18:22:30 -0300

    refactor: adjusting env service to add dry to this

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,15 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
-import { Env } from './env';
+import { EnvService } from './env/env.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'debug'],
   });
 
-  const configService = app.get<ConfigService<Env, true>>(ConfigService);
-  const port = configService.get('PORT', { infer: true });
+  const envService = app.get(EnvService);
+  const port = envService.get('PORT');
 
   await app.listen(port, () => console.log(`Server is running on port ${port}`));
 }
