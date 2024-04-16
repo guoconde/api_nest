5b32ed27af32642f21913dfc571c032a00bd3ebf
guoconde <guoconde@gmail.com>
2024-04-16 07:49:14 -0300

    feat: implements tests to redis

--- test/config/setup-e2e.ts
+++ test/config/setup-e2e.ts
@@ -4,18 +4,28 @@ import { PrismaClient } from '@prisma/client';
 import { randomUUID } from 'node:crypto';
 import { execSync } from 'node:child_process';
 import { DomainEvents } from '@/core/events/domain-events';
+import { envSchema } from '@/infra/env/env';
+import { Redis } from 'ioredis';
 
 config({ path: '.env', override: true });
 config({ path: '.env.test', override: true });
 
+const env = envSchema.parse(process.env);
+
 const prisma = new PrismaClient();
 
+const redis = new Redis({
+  host: env.REDIS_HOST,
+  port: env.REDIS_PORT,
+  db: env.REDIS_DB,
+});
+
 const generateUniqueDatabaseURL = (schemaId: string) => {
-  if (!process.env.DATABASE_URL) {
+  if (!env.DATABASE_URL) {
     throw new Error('DATABASE_URL is not set');
   }
 
-  const url = new URL(process.env.DATABASE_URL);
+  const url = new URL(env.DATABASE_URL);
 
   url.searchParams.set('schema', schemaId);
 
@@ -29,9 +39,11 @@ beforeAll(async () => {
 
   const databaseURL = generateUniqueDatabaseURL(schemaId);
 
-  process.env.DATABASE_URL = databaseURL;
+  env.DATABASE_URL = databaseURL;
 
   DomainEvents.shouldRun = false;
+
+  await redis.flushdb();
 
   execSync('npx prisma migrate deploy');
 });
